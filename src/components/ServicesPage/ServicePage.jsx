70661ecba5d482dc9f019210af9b97e2e5337177
Your username <Your email address>
2023-11-26 01:19:43 -0800

    add: picture

--- src/components/ServicesPage/ServicePage.jsx
+++ src/components/ServicesPage/ServicePage.jsx
@@ -1,6 +1,7 @@
 import Header from "../LandingPage/Header";
 import HomeFooter from "../LandingPage/HomeFooter";
 import ServiceSection1 from "./ServiceSection1";
+import ServiceSection2 from "./ServiceSection2";
 import "/src/css/services.css";
 
 const ServicePage = () => {
@@ -8,6 +9,7 @@ const ServicePage = () => {
     <>
       <Header />
       <ServiceSection1 />
+      <ServiceSection2 />
       <HomeFooter />
     </>
   );
---
+++ src/components/ServicesPage/ServiceSection2.jsx
@@ -0,0 +1,79 @@
+const ServiceSection2 = () => {
+  return (
+    <>
+      <section className="servicesection2">
+    
+        <div className="box">
+          <h1>Popular Features</h1>
+          <div className="innerbox">
+            <div className="box1">
+              <h6>FFP CRPTO INDEX</h6>
+              <h4>Auto-invest in the whole crypto market</h4>
+              <button>Get Started</button>
+              <img src="" alt="" />
+            </div>
+            <div className="box2">
+              <h6>SAVING PLANS</h6>
+              <h4>Save effortlessly with a savings plan</h4>
+              <button>Get Started</button>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <h1>Benefits and Rewards</h1>
+          <div className="innerbox">
+            <div className="box1">
+              <h6>CASHBACK</h6>
+              <h4>Cashback in Cryptocurrencies</h4>
+              <button>Learn More</button>
+              <img src="" alt="" />
+            </div>
+            <div className="box2">
+              <h6>BEST REWARDS</h6>
+              <h4>Earn rewards and exclusive perks</h4>
+              <button>Learn More</button>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <h1>Save Time and Money</h1>
+          <div className="innerbox">
+            <div className="box1">
+            <h6>SWAP</h6>
+              <h4>Exchange digital assets instantly</h4>
+              <button>Learn More</button>
+              <img src="" alt="" />
+            </div>
+            <div className="box2">
+            <h6>PAYMENTS</h6>
+              <h4>send money with FFB</h4>
+              <button>Learn More</button>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <h1>Make money</h1>
+          <div className="innerbox">
+            <div className="box1">
+            <h6>TELL-A-FRIEND PROGRAMME</h6>
+              <h4>Invite your friends, earn rewards</h4>
+              <button>Learn More</button>
+              <img src="" alt="" />
+            </div>
+            <div className="box2">
+            <h6>AFFILIATE PROGRAMME</h6>
+              <h4>Become a partner and earn commission</h4>
+              <button>Learn More</button>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ServiceSection2;
