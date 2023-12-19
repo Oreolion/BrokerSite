7b603a20b965dcd5e2d461bf8bbb28b72bcfaccf
Your username <Your email address>
2023-12-19 02:42:50 -0800

    add: pricing page t

--- src/components/PricingPage/PricingPage.jsx
+++ src/components/PricingPage/PricingPage.jsx
@@ -1,11 +1,21 @@
 import Header from "../LandingPage/Header";
 import HomeFooter from "../LandingPage/HomeFooter";
+import TradingPlan from "../LandingPage/TradingPlan";
+import HomePageSection4 from "../LandingPage/HomePageSection4";
+import PricingSection1 from "./PricingSection1";
+import "/src/css/pricing.css";
+
 
 const PricingPage = () => {
   return (
     <>
       <Header />
+      <PricingSection1 />
+      <h1 className="pricingh1">AFFORDABLE PACKAGES</h1>
+      <TradingPlan />
+      <HomePageSection4 />
       <HomeFooter />
+
     </>
   );
 };
---
+++ src/components/PricingPage/PricingSection1.jsx
@@ -0,0 +1,12 @@
+const PricingSection1 = () => {
+  return (
+    <>
+    <section className="pricingsection1">
+        <h1>OUR <span>PRICES</span></h1>
+        <p>HOME <span>/ PRICING</span></p>
+    </section>
+    </>
+  )
+}
+
+export default PricingSection1;
