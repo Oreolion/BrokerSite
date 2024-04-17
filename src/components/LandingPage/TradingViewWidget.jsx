f9c304a54d7058da0c1b67091f79916de182fb2e
oreolion <remyoreo11@gmail.com>
2024-04-17 03:47:01 -0700

    add: fix bug in routing;

--- src/components/LandingPage/TradingViewWidget.jsx
+++ src/components/LandingPage/TradingViewWidget.jsx
@@ -1,62 +1,66 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 const TradingViewWidget = () => {
-    const myRef = useRef(null)
+  const myRef = useRef(null);
   useEffect(() => {
     // Insert the TradingView Widget script dynamically
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js';
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js";
     script.async = true;
     script.innerHTML = JSON.stringify({
-      "symbols": [
+      symbols: [
         {
-          "proName": "FOREXCOM:SPXUSD",
-          "title": "S&P 500"
+          proName: "FOREXCOM:SPXUSD",
+          title: "S&P 500",
         },
         {
-          "proName": "FOREXCOM:NSXUSD",
-          "title": "US 100"
+          proName: "FOREXCOM:NSXUSD",
+          title: "US 100",
         },
         {
-          "proName": "FX_IDC:EURUSD",
-          "title": "EUR to USD"
+          proName: "FX_IDC:EURUSD",
+          title: "EUR to USD",
         },
         {
-          "proName": "BITSTAMP:BTCUSD",
-          "title": "Bitcoin"
+          proName: "BITSTAMP:BTCUSD",
+          title: "Bitcoin",
         },
         {
-          "proName": "BITSTAMP:ETHUSD",
-          "title": "Ethereum"
-        }
+          proName: "BITSTAMP:ETHUSD",
+          title: "Ethereum",
+        },
       ],
-      "showSymbolLogo": true,
-      "colorTheme": "light",
-      "isTransparent": false,
-      "displayMode": "adaptive",
-      "locale": "en"
+      showSymbolLogo: true,
+      colorTheme: "light",
+      isTransparent: false,
+      displayMode: "adaptive",
+      locale: "en",
     });
 
     myRef.current.appendChild(script);
 
     return () => {
       // Cleanup: remove the script tag if the component unmounts
-      myRef.current.removeChild(script);
+      if (myRef.current) {
+        myRef.current.removeChild(script);
+      }
     };
   }, []);
 
   return (
-    <div className="widgetbox"  ref={myRef}>
-
-    <div className="tradingview-widget-container">
-      <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright">
-        <a href="https://www.tradingview.com/" rel="noopener nofollow noreferrer" target="_blank">
-          {/* <span className="blue-text">Track all markets on TradingView</span> */}
-        </a>
+    <div className="widgetbox" ref={myRef}>
+      <div className="tradingview-widget-container">
+        <div className="tradingview-widget-container__widget"></div>
+        <div className="tradingview-widget-copyright">
+          <a
+            href="https://www.tradingview.com/"
+            rel="noopener nofollow noreferrer"
+            target="_blank"
+          ></a>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
